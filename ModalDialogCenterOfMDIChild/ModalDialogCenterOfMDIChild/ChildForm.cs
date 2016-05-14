66c18345fac13fc6c5b5cadd56847eedbdd7b295
CoolBots <stan@buildcoolrobots.com>
2016-05-14 13:04:07 -0700

    How to center a ModalDialog to an MDI Child form
    This is an MDI (Multiple Document Interface) project in C# / Windows
    Forms. The project demonstrates how to center a Windows Form as a
    ModalDialog in the center of another Windows Form (the invoking form),
    which is in turn an MDI Child, located anywhere within the MDI Parent
    container.

---
+++ ModalDialogCenterOfMDIChild/ModalDialogCenterOfMDIChild/ChildForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModalDialogCenterOfMDIChild
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void invokeDialogButton_Click(object sender, EventArgs e)
+        {
+            var dialogForm = new DialogForm();
+            dialogForm.StartPosition = FormStartPosition.Manual;
+
+            //Get the actual position of the MDI Parent form in screen coords
+            Point screenLocation = Parent.PointToScreen(Parent.Location);
+
+            //Adjust for position of the MDI Child form in screen coords
+            screenLocation.X += Location.X;
+            screenLocation.Y += Location.Y;
+
+            dialogForm.Location = new Point(screenLocation.X + (Width - dialogForm.Width) / 2,
+                                            screenLocation.Y + (Height - dialogForm.Height) / 2);
+
+            dialogForm.ShowDialog(this);
+        }
+    }
+}
---
+++ ModalDialogCenterOfMDIChild/ModalDialogCenterOfMDIChild/ParentForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModalDialogCenterOfMDIChild
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+            ChildForm form = new ChildForm();
+            form.MdiParent = this;
+            form.Show();
+        }
+    }
+}
